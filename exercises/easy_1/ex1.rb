b1859f8b6ba64f41c12f761ff9a020af78d70d18
Kevin Hechavarria <kevnhech@gmail.com>
2021-03-11 17:30:24 -0800

    Updating easy_1 exercises

--- exercises/easy_1/ex1.rb
+++ exercises/easy_1/ex1.rb
@@ -1,5 +1,26 @@
-def repeat(string, integer)
-  puts (string + "\n") * integer
+# Write a method that takes two arguments, a string and a positive integer, and prints the string as many times as the integer indicates.
+
+# repeat('Hello', 3)
+# Outputs: `Hello`, `Hello`, `Hello`
+
+# input: a string and an integer
+# output: a string that is returned `integer` times
+# rules: the `repeat` method takes two arguments: a string and a positive integer
+#        the method prints the string as many times as the integer indicates
+
+# initialize local variable `count` and assign it to integer `0`
+# invoke method `loop`
+#   return the string
+#   increment `count` by 1
+#   stop the loop once `count` equals to the integer
+
+def repeat(str, num)
+  count = 0
+  loop do
+    puts str
+    count += 1
+    break if count == num
+  end
 end
 
 repeat('Hello', 3)
--- exercises/easy_1/ex2.rb
+++ exercises/easy_1/ex2.rb
@@ -1,24 +1,28 @@
-def is_odd?(num)
-  if num % 2 == 0
-    puts false
-  else
-    puts true
-  end
-end
+# Write a method that takes one integer argument, which may be positive, negative, or zero. This method returns true if the number's absolute value is odd. You may assume that the argument is a valid integer value.
 
-puts is_odd?(2)
-puts is_odd?(5)
-puts is_odd?(-17)
-puts is_odd?(-8)
-puts is_odd?(0)
-puts is_odd?(7)
+# input: any integer
+# output: booleans true or false
+# rules: returns `true` if the number's absolute value is odd, false otherwise
 
-=begin
+# Compare the result of integer modulo `2` with integer `1` to see if it's odd or not
 
 def is_odd?(num)
-  num.remainder(2) == 1
+  num % 2 == 1
 end
 
-is_odd?(5) # This will output true.
+puts is_odd?(2)    # => false
+puts is_odd?(5)    # => true
+puts is_odd?(-17)  # => true
+puts is_odd?(-8)   # => false
+puts is_odd?(0)    # => false
+puts is_odd?(7)    # => true
 
-=end
+# Further Exploration
+
+# If you weren't certain whether % were the modulo or remainder operator, how would you rewrite #is_odd? so it worked regardless?
+
+# Call method `#remainder` on integer, passing in `2` as an argument, then compare the return value to integer `1`
+
+def is_odd?(num)
+  num.remainder(2) == 1 || num.remainder(2) == -1
+end
